b11bef35a0583b634c52438f3712e3182b53fa9b
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2021-01-03 15:09:28 +0200

    add location and max/mix order

---
+++ class_06/app.js
@@ -0,0 +1,29 @@
+'use strict';
+var Seattle = {
+    minCust: '23',
+    maxCust: '65',
+    avgCookieSale: '6.3'
+    randomNum: function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+};
+var Tokyo = {
+    minCust: '3',
+    maxCust: '24',
+    avgCookieSale: '1.2'
+};
+var Dubai = {
+    minCust: '11',
+    maxCust: '38',
+    avgCookieSale: '3.7'
+};
+var Paris = {
+    minCust: '20',
+    maxCust: '38',
+    avgCookieSale: '2.4'
+};
+var Lima = {
+    minCust: '2',
+    maxCust: '16',
+    avgCookieSale: '4.6'
+};
